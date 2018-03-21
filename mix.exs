863b393b2420a670062b9720515f3bdd3ea4e6d1
Thibaut Mattio <thibaut.mattio@gmail.com>
2018-03-20 20:13:19 -0400

    Update absinthe version to 1.4.9

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Monkey.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:absinthe, "~> 1.4.8"},
+      {:absinthe, "~> 1.4.9"},
       {:absinthe_ecto, "~> 0.1.3"},
       {:absinthe_plug, "~> 1.4.2"},
       {:poison, "~> 3.1.0"},
